b5f52e268243fc71b0578e360a4affbaebb655b0
Abhinav Rajesh <abhinavrajesh@github.com>
2023-03-23 21:41:45 +0000

    chore: added voice channel to example

--- examples/livingRoom/client/src/App.tsx
+++ examples/livingRoom/client/src/App.tsx
@@ -3,10 +3,11 @@ import "./App.css";
 import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import { Canvas, useThree, useLoader, useFrame } from "@react-three/fiber";
-import Controller from "./utils/controller";
+import Controller from "./utils/Controller";
 import Avatars from "./utils/Avatars";
 import Room from "./Room";
 import { SERVER } from "./infrastructure";
+import AudioChannel from "./Audio";
 
 function App() {
   const [avatarPositions, setAvatarPositions] = useState([
@@ -46,6 +47,7 @@ function App() {
           <PerspectiveCamera position={[0, 0, 0]} makeDefault manual />
           <Controllers />
           <Hands />
+          <AudioChannel />
           {/* <OrbitControls
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={0}
