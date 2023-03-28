844415fcccac157758e11e8b4b022d5fee431497
Abhinav Rajesh <abhinavrajesh@github.com>
2023-03-28 11:27:30 +0000

    feat: switch to toolkit for coordinates

--- examples/livingRoom/client/src/common/config.ts
+++ examples/livingRoom/client/src/common/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  WS_API: `ws://127.0.0.1:8080`,
+  WS_API: `wss://abhinavrajesh-orange-space-waffle-rw5rg5456qfxgj6-8080.preview.app.github.dev`,
 };
 
 export default config;
--- examples/livingRoom/client/src/utils/Controller.tsx
+++ examples/livingRoom/client/src/utils/Controller.tsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import { SERVER } from "../infrastructure";
+import { io } from "socket.io-client";
 
 const Controller = () => {
   const [paused, setPaused] = useState<boolean>(false);
+  const socket = io("https://worried-pie-production.up.railway.app")
 
   useFrame(({ gl, camera }) => {
     if (gl.xr.getSession() !== null && !paused) {
-      console.log("useFrame:", camera.position);
-      SERVER.send({
+      socket.emit("coordinates", JSON.stringify({
         type: "coordinates",
-        position: camera.position,
-      });
+        position: camera.position
+      }))
       setPaused(() => true);
     }
   });
